03d8e8ded0f74aaab580f85947007439e2f37c6e
k <kubaloj222@gmail.com>
2026-05-10 11:34:51 +0200

    zadanie 4

--- zadanie4formularz/MainPage.xaml.cs
+++ zadanie4formularz/MainPage.xaml.cs
@@ -6,16 +6,49 @@
         List<string> poziomy2 = new List<string> { "Niski", "Średni" };
 
         List<string> Tematy = new List<string> {"Reklamacja","Podziękowanie","Pytanie","Inne" };
+
+        string priorytet,title;
+        
         public MainPage()
         {
             InitializeComponent();
-
+            tematPicker.ItemsSource = Tematy;
+            priorPicker.ItemsSource = poziomy1;
         }
-        public void priotytetchanged()
+        public void tematchanged(object sender, EventArgs e)
         {
+            priorPicker.IsEnabled = true;
+            var picker = (Picker)sender;
+            title = (string)picker.SelectedItem;
+            if (title == "Reklamacja")
+            {
+                priorPicker.ItemsSource = poziomy1;
+            }
+            else if (title=="Pytanie")
+            {
+                priorPicker.ItemsSource = poziomy2;
+            }
+            else
+            {
+                priorytet = "Domyślny";
+                priorPicker.IsEnabled = false;
+            }
+        }
+        public void priorytetchanged(object sender , EventArgs e)
+        {
+            var picker = (Picker)sender;
+            priorytet = (string)picker.SelectedItem;
 
         }
-        public void tematchanged(object sender, )
+        public void OnMessageClicked(object sender,EventArgs e)
+        {
+            string kopiamessage;
+            bool kopia = copyCheckbox.IsChecked;
+            if (kopia) kopiamessage = "Tak";
+            else kopiamessage = "Nie";
+                string message = $" {nameEntry.Text} \n {mailEntry.Text} \n Wysłać kopię na email:{kopiamessage}\n Temat: {title}    priorytet: {priorytet} ";
+            DisplayAlert("Wiadomość wysłana pomyślnie",message, "Ok");
+        }
         
     }
 }
